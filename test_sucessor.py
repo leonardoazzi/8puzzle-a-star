aba096986c1677fb827aa5790b2e7562fe14f0fd
Leonardo Azzi Martins <leoazzim@gmail.com>
2024-01-21 17:35:32 -0300

    Change aux.py to gameLogic.py

--- test_sucessor.py
+++ test_sucessor.py
@@ -1,4 +1,4 @@
-from aux import sucessor
+from gameLogic import sucessor
 
 def test_sucessor():
     assert sucessor("2_3541687") == {('abaixo', '2435_1687'), ('esquerda', '_23541687'), ('direita', '23_541687')}
